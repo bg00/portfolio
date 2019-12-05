4611dc9669c3c5b070feb380d6e6525283fcc37a
Bailey Garrett <baileyagarrett@gmail.com>
2019-12-04 19:22:49 -0600

    commenting out the js file

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // responsive design
 
-function checkBreakPoint() {
-  var w = Math.max(
-    document.documentElement.clientWidth,
-    window.innerWidth || 0
-  );
-  toggleNavLocation(w);
-}
+// function checkBreakPoint() {
+//  var w = Math.max(
+//    document.documentElement.clientWidth,
+//    window.innerWidth || 0
+//  );
+//  toggleNavLocation(w);
+// }
 
-function toggleNavLocation(screensize) {
-  if (screensize > 999) {
-    if (mainNav.parentElement.id === "h1") {
-      branding.appendChild(body);
-    }
-  } else {
-    if (mainNav.parentElement.id === "div") {
-      tieredHeader.appendChild(body);
-    }
-  }
-}
+// function toggleNavLocation(screensize) {
+ // if (screensize > 999) {
+ //   if (mainNav.parentElement.id === "h1") {
+ //     branding.appendChild(body);
+ //   }
+ // } else {
+ //   if (mainNav.parentElement.id === "div") {
+ //     tieredHeader.appendChild(body);
+ //   }
+ //  }
+// }
